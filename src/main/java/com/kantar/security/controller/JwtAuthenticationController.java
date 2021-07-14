95abcb0df88ae783a4b6479c73d72975cd1f5ec5
Ramakanthkantar <84090245+Ramakanthkantar@users.noreply.github.com>
2021-07-14 11:26:55 +0530

    updates with new file

---
+++ src/main/java/com/kantar/security/controller/JwtAuthenticationController.java
@@ -0,0 +1,74 @@
+package com.kantar.security.controller;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kantar.security.config.JwtTokenUtil;
+import com.kantar.security.model.JwtRequest;
+import com.kantar.security.model.JwtResponse;
+import com.kantar.security.model.UserDTO;
+import com.kantar.security.model.UserResponse;
+import com.kantar.security.service.JwtUserDetailsService;
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@Autowired
+	private JwtUserDetailsService userDetailsService;
+	
+	/*
+	 * @RequestMapping({ "/hello" }) public String firstPage() { return
+	 * "Hello World"; }
+	 */
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public UserResponse saveUser(@RequestBody UserDTO user) throws Exception {
+	userDetailsService.save(user);	
+		
+	return new UserResponse( " inserted", Boolean.TRUE);
+		
+		
+		//return ResponseEntity.ok(userDetailsService.save(user));
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+}
